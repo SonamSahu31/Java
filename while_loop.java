091c66d732553af26296f9065344db71525f0b2c
Sonam Sahu <sonamsahu3101@gmail.com>
2026-07-18 23:40:31 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int count = 0; // Initialize count
+        while (count < 5) { // Loop until count is less than 5
+            System.out.println("Count is: " + count);
+            count++; // Increment count
+        }
+    }
+}
