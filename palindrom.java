802203206bfce18ee46e2fd3b5053fd9c499c822
Sonam Sahu <sonamsahu3101@gmail.com>
2026-08-09 23:37:42 +0530

    Add files via upload

---
+++ palindrom.java
@@ -0,0 +1,15 @@
+package recursion;
+public class palindrom {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+
+        if (str.charAt(start) != str.charAt(end)) return false;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println(isPalindrome(str, 0, str.length() - 1));
+    }
+}
