5adc6a4cec87965149f982788554d06d89346954
Sonam Sahu <sonamsahu3101@gmail.com>
2026-08-25 23:53:51 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,27 +1,20 @@
-package recursion;
-
 public class reverse {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        reverseArray(arr, 0, arr.length - 1);
-
-        System.out.println("Reversed Array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
+    public static void main(String [] agrs){
+        int arr[] = {1,2,3,4,5};
+        int start = 0;
+        int end = arr.length-1;
+        int temp;
+        while(start  < end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-    }
-
-    public static void reverseArray(int[] arr, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        // swap
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        reverseArray(arr, start + 1, end - 1);
+       System.out.println("Reversed Array is: "); 
+       for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] + " ");
+       }
     }
     
 }
