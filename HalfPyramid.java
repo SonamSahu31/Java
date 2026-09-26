823fa1fa991b71ec037af1a5ad954aa660c0bb8d
Sonam Sahu <sonamsahu3101@gmail.com>
2026-09-26 23:48:47 +0530

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class HalfPyramid {
+    public static void main(String[] args){
+        int n =5;
+        for(int i=1; i<=n; i++){ // outer loop for rows
+            for(int j=1; j<=i; j++){ // inner loop for stars
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
